3426f3879af18c6d5cf6a26cb655e2c3dc89e1b4
Samrath Singh Saini <200446420@student.georgianc.on.ca>
2021-04-18 00:31:10 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fantastical_Events_2021.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
