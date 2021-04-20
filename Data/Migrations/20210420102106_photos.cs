2b4e68268aa067e6d38078670b1b7fd98e2f308f
Samrath Singh Saini <200446420@student.georgianc.on.ca>
2021-04-20 06:21:30 -0400

    ...

---
+++ Data/Migrations/20210420102106_photos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fantastical_Events_2021.Data.Migrations
+{
+    public partial class photos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
